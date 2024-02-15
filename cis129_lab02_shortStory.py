734d29d1cab2d8e3a14b0b95576377edb35f07fb
mzambrano6 <160148598+mzambrano6@users.noreply.github.com>
2024-02-15 08:50:29 -0700

    cis-129-shortStory

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,9 @@
-print('Hello, World!')
+#Author: Mizaiel Zambrano
+#My first Pythoin lab in my CIS129 course
+#Prints a simple short story
+
+print('Once upon a time there was a big human that liked to make music and work on computers.')
+print('One day a little human came around and the big human taught the little one all he knew about computers')
+print('The little one took a huge interest in computers and loved to take computers apart and fix them')
+print('The little one, not so little anymore loved his passion for computers.')
+
